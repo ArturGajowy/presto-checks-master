f3df6bba88e46f3c0b1b05cafef60fb0b1a97dd7
Artur Gajowy <artur.gajowy@gmail.com>
2016-08-24 17:10:55 +0200

    Scripts for triggering the build

--- trigger_checks_for_latest_build.sh
+++ trigger_checks_for_latest_build.sh
@@ -1,40 +1,38 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 3 || $# -ne 4 ]]; then
-  echo 'Usage: trigger_checks_for_latest_build.sh $CHECKS_GITHUB_REPO $CHECKS_REPO_TRAVIS_TOKEN $BRANCH [$CHECKS_BRANCH]'
+if [[ $# -ne 4 ]]; then
+  echo 'Usage: trigger_checks_for_latest_build.sh $CHECKS_GITHUB_REPO $CHECKS_REPO_TRAVIS_TOKEN $CHECKS_BRANCH $PRESTO_BRANCH'
   echo ''
-  echo 'Passing $BRANCH = `@{LATEST_SPRINT_BRANCH}` will trigger checks for the latest sprint branch.'
-  echo 'Optional $CHECKS_BRANCH will default to `master`, or `sprint` if $BRANCH = @{LATEST_SPRINT_BRANCH}.'
+  echo 'Passing $PRESTO_BRANCH = `@{LATEST_SPRINT_BRANCH}` will trigger checks for the latest sprint branch.'
   exit 1
 fi
 
 CHECKS_GITHUB_REPO=${1/\//%2F}
 CHECKS_REPO_TRAVIS_TOKEN=$2
-BRANCH=$3
-CHECKS_BRANCH=${CHECKS_BRANCH:-master}
+CHECKS_BRANCH=$3
+PRESTO_BRANCH=$4
 
 ARTIFACTS_S3_BUCKET='teradata-presto'
 ARTIFACTS_S3_PATH='travis_build_artifacts/Teradata/presto'
 
-if [["$BRANCH" == '@{LATEST_SPRINT_BRANCH}' ]]; then
-    BRANCH=`aws s3 ls s3://${ARTIFACTS_S3_BUCKET}/${ARTIFACTS_S3_PATH} --no-sign-request | awk '{print $2}' | sed 's/.$//' | egrep '^sprint-[0-9]+$' | sort -n -t '-' -k 2 | tail -n1`
-    echo "Current sprint branch resolved to [${BRANCH}]"
-    CHECKS_BRANCH=${CHECKS_BRANCH:-sprint}
+if [[ "$PRESTO_BRANCH" == '@{LATEST_SPRINT_BRANCH}' ]]; then
+    PRESTO_BRANCH=`aws s3 ls s3://${ARTIFACTS_S3_BUCKET}/${ARTIFACTS_S3_PATH}/ --no-sign-request | awk '{print $2}' | sed 's/.$//' | egrep '^sprint-[0-9]+$' | sort -n -t '-' -k 2 | tail -n1`
+    echo "Current sprint branch resolved to [${PRESTO_BRANCH}]"
 fi
 
-BUILD=`aws s3 ls s3://${ARTIFACTS_S3_BUCKET}/${ARTIFACTS_S3_PATH}/$BRANCH/ --no-sign-request | awk '{print $2}' | sed 's/.$//' | sort -n | tail -n1`
+PRESTO_BUILD=`aws s3 ls s3://${ARTIFACTS_S3_BUCKET}/${ARTIFACTS_S3_PATH}/$PRESTO_BRANCH/ --no-sign-request | awk '{print $2}' | sed 's/.$//' | sort -n | tail -n1`
 
 BODY=$(cat << EOF
 {
   "request": {
-    "message": "Checks for branch [$BRANCH] build [$BUILD]",
-    "branch": "$CHECKS_BRANCH"
+    "message": "Checks for Presto branch [$PRESTO_BRANCH] build [$PRESTO_BUILD]",
+    "branch": "$CHECKS_BRANCH",
     "config": {
       "before_install": [
         "export ARTIFACTS_S3_BUCKET='$ARTIFACTS_S3_BUCKET'",
         "export ARTIFACTS_S3_PATH='$ARTIFACTS_S3_PATH'",
-        "export BRANCH='$BRANCH'",
-        "export BUILD='$BUILD'"
+        "export PRESTO_BRANCH='$PRESTO_BRANCH'",
+        "export PRESTO_BUILD='$PRESTO_BUILD'"
       ]
     }
   }
